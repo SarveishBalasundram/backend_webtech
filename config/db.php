682028ede93d3db256ca8421052ace97c4ab110b
B.Sarveish <121470881+SarveishBalasundram@users.noreply.github.com>
2025-06-21 11:54:01 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "sql12.freesqldatabase.com";
+$db_host = "sql12.freesqldatabase.com";
 $port = 3306; // Default MySQL port. Replace if your provider uses a different port.
-$dbname = "sql12786002";
-$username = "sql12786002";
-$password = "nbGpSqimCX";
+$db_name = "sql12786002";
+$db_user = "sql12786002";
+$db_pass = "nbGpSqimCX";
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$db_host;port=$port;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
